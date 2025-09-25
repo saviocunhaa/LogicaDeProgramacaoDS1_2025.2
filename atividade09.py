d941791a2bff3739f0e90392e8ac55aa081ed2e1
saviocunhaa <saviocunha.01@hotmail.com>
2025-09-25 14:05:09 -0300

    lista 01

---
+++ atividade09.py
@@ -0,0 +1,3 @@
+# Atividade 09 – Cálculo de Área
+# Peça ao usuário a largura e a altura de um retângulo.
+# Calcule e mostre a área total.
---
+++ atividade10.py
@@ -0,0 +1,3 @@
+# Atividade 10 – Conversão de Unidades
+# Peça ao usuário um valor em metros.
+# Converta e mostre o resultado em centímetros e milímetros.
