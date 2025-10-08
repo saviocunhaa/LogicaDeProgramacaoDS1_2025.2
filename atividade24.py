0b2d7b32beb02d9cbd3a33539b6ee5986a1b116b
saviocunhaa <saviocunha.01@hotmail.com>
2025-10-08 14:46:47 -0300

    organizar 24

---
+++ atividade24.py
@@ -0,0 +1,5 @@
+# Validação de Acesso a um Sistema Interno
+# Peça o cargo e o turno do funcionário.
+# Apenas administradores que trabalham no turno da manhã podem acessar o painel de controle.
+# Use operadores lógicos para validar.
+# Use operadores lógicos para validar.
--- atividade40.py
+++
@@ -1,4 +0,0 @@
-# Validação de Acesso a um Sistema Interno
-# Peça o cargo e o turno do funcionário.
-# Apenas administradores que trabalham no turno da manhã podem acessar o painel de controle.
-# Use operadores lógicos para validar.
